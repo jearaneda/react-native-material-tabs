c01d5bdcb2105569c7a3d15b348579fe235be05d
Jorge Macbook <jearaneda@uc.cl>
2020-07-15 18:15:42 -0400

    width as padding

--- src/components/MaterialTabs.tsx
+++ src/components/MaterialTabs.tsx
@@ -163,7 +163,7 @@ const MaterialTabs: React.FC<Props> = ({
                 activeTextStyle={selectedIndex === idx && activeTextStyle}
                 tabHeight={barHeight}
                 tabPadding={tabPadding}
-                tabWidth={!scrollable ? tabWidth : barWidth * 0.3}
+                tabWidth={tabPadding}
                 uppercase={uppercase}
                 inActiveTextColor={inactiveTextColor}
               />
@@ -173,7 +173,7 @@ const MaterialTabs: React.FC<Props> = ({
           <Indicator
             color={indicatorColor}
             value={indicatorPosition}
-            tabWidth={!scrollable ? tabWidth : barWidth * 0.3}
+            tabWidth={tabPadding}
           />
         </ScrollView>
       </Bar>
